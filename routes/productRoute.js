780f01c11b99564b0499141abdf1018f50e68d79
Anurag-Dhurwey <anuragdhurwey9211@gmail.com>
2024-01-14 23:19:10 +0530

    some routes removed

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,13 +3,13 @@ const {
   createProduct,
   getaProduct,
   getAllProduct,
-  getDraftProducts,
+  // getDraftProducts,
   updateProduct,
   deleteProduct,
   addToWishlist,
   rating,
-  getProductByCategory,
-  deals_of_the_day,
+  // getProductByCategory,
+  // deals_of_the_day,
 } = require("../controller/productCtrl");
 const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
@@ -24,8 +24,8 @@ router.put("/:id", authMiddleware, isAdmin, updateProduct);
 router.delete("/:id", authMiddleware, isAdmin, deleteProduct);
 
 router.get("/", getAllProduct);
-router.get("/draft/products", getDraftProducts);
-router.get("/category/:category", getProductByCategory);
-router.get("/deals/deals_of_the_day", deals_of_the_day);
+// router.get("/draft/products", getDraftProducts);
+// router.get("/category/:category", getProductByCategory);
+// router.get("/deals/deals_of_the_day", deals_of_the_day);
 
 module.exports = router;
