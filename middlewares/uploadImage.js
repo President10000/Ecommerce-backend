50409e4e5cfe9650249feb059882513a2ba8b22a
vishal patel <vishallovevap@gmail.com>
2024-01-16 19:23:40 +0530

    multer configure

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "../public/images/"));
+    cb(null, path.join(__dirname, "/tmp/images"));
   },
   filename: function (req, file, cb) {
     const uniquesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
