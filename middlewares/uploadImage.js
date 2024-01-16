e0959e14c5edd97fd21e32f3152b889e210de623
vishal patel <vishallovevap@gmail.com>
2024-01-16 19:35:59 +0530

    make dir solve

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -4,8 +4,9 @@ const path = require("path");
 const fs = require("fs");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "/tmp/images"));
+    const dir = path.join(__dirname, "/tmp/images");
     fs.mkdirSync(dir, { recursive: true });
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
     const uniquesuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
